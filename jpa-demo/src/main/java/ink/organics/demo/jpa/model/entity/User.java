58de83ca024fc0bca2ac533f43c1b28ee062e11f
organics <374564811@qq.com>
2022-07-27 14:13:47 +0800

    update

--- jpa-demo/src/main/java/ink/organics/demo/jpa/model/entity/User.java
+++ jpa-demo/src/main/java/ink/organics/demo/jpa/model/entity/User.java
@@ -4,6 +4,7 @@ package ink.organics.demo.jpa.model.entity;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,6 +16,7 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDHexGenerator")
+    @Column(columnDefinition = "char", length = 32)
     private String id;
     private String name;
     private Integer age;
--- jpa-demo/src/main/resources/db/schema-mysql.sql
+++ jpa-demo/src/main/resources/db/schema-mysql.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `created_date` datetime(6) NULL DEFAULT NULL,
   `last_modified_date` datetime(6) NULL DEFAULT NULL,
   `age` int(0) NULL DEFAULT NULL,
